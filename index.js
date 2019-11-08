1cdabf2b7c2b5538d13bed8685190241c592df3f
eric.chang <eric@wethink.com.tw>
2019-11-08 14:14:58 +0800

    'update'

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const express = require('express'),
 
 const mysql = require('mysql');
 // connection configurations
-const mc = mysql.createConnection({
-    host     : '35.67.195.119',
-    user     : 'eric',
-    password : '1234',
-    database : 'askmequestion'
-});
+// const mc = mysql.createConnection({
+//     host     : '35.67.195.119',
+//     user     : 'eric',
+//     password : '1234',
+//     database : 'askmequestion'
+// });
  
+var connection = mysql.createConnection({ 
+    sockePath : '/cloudsql/ultra-concord-258108:us-central1:reporter-trackere', user : 'eric', 
+password : '1234', database : 'askmequestion' });
+
 // connect to database
-mc.connect();
+connection.connect();
 
 app.listen(port);
 
